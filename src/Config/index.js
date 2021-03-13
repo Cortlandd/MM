8a6a015f0a5ca5f6da983138a7cee2c11a4e9c47
Cortland Walker <cortlandjwalker@icloud.com>
2021-03-12 21:21:41 -0600

    Added messaging platforms to config.

--- src/Config/index.js
+++ src/Config/index.js
@@ -3,7 +3,14 @@ const ContainerNames = {
   NewConversation: 'New Conversation',
 }
 
+const MessengerPlatforms = {
+  Instagram: 'Instagram',
+  FacebookMessenger: 'Facebook Messenger',
+  iMessage: 'iMessage',
+}
+
 export const Config = {
   API_URL: 'https://jsonplaceholder.typicode.com/',
   containerNames: ContainerNames,
+  messengerPlatforms: MessengerPlatforms,
 }
