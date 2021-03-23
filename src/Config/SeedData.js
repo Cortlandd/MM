fb13254eafe4cb95f9db292c8335b1fbe2b6d5d1
Cortland Walker <cortlandjwalker@icloud.com>
2021-03-22 21:00:16 -0500

    Implemented seed data for testing conversations.

--- src/Config/SeedData.js
+++ src/Config/SeedData.js
@@ -2,6 +2,59 @@ import Images from '@/Theme/Images'
 
 const images = Images()
 
+const TestConversation = [
+  {
+    id: 1,
+    time: Date.now(),
+    message: 'Hello',
+    type: 'sent',
+    recipient: {
+      name: 'Vito Corleone',
+      image: images.FacebookMessenger,
+    },
+  },
+  {
+    id: 2,
+    time: Date.now(),
+    message: 'My name is Cortland',
+    type: 'sent',
+    recipient: {
+      name: 'Vito Corleone',
+      image: images.FacebookMessenger,
+    },
+  },
+  {
+    id: 3,
+    time: Date.now() + 10,
+    message: 'Hello Cortland, how can I help you?',
+    type: 'received',
+    recipient: {
+      name: 'Vito Corleone',
+      image: images.FacebookMessenger,
+    },
+  },
+  {
+    id: 4,
+    time: Date.now() + 10,
+    message: 'Just saying hi. Wanted to introduce myself. and testing this',
+    type: 'sent',
+    recipient: {
+      name: 'Vito Corleone',
+      image: images.FacebookMessenger,
+    },
+  },
+  {
+    id: 5,
+    time: Date.now() + 10,
+    message: 'Okay',
+    type: 'received',
+    recipient: {
+      name: 'Vito Corleone',
+      image: images.FacebookMessenger,
+    },
+  },
+]
+
 const TestConversationList = [
   {
     recipient: {
@@ -40,4 +93,5 @@ const TestConversationList = [
 
 export const SeedData = {
   TestConversationList: TestConversationList,
+  TestConversation: TestConversation,
 }
