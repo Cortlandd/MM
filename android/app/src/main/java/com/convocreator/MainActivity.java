025c560a1a95bff28ba099634e011e1c0a4f3687
Cortland Walker <cortlandjwalker@icloud.com>
2021-05-10 08:51:15 -0500

    Rename app to Msg MAker

--- android/app/src/main/java/com/convocreator/MainActivity.java
+++ android/app/src/main/java/com/convocreator/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "ConvoCreator";
+    return "MsgMaker";
   }
 
   // for react-native-appearance
