f30bbc52079a92d38bc850e5d2bf83509966b568
Cortland Walker <cortlandjwalker@icloud.com>
2021-06-05 12:09:44 -0500

    Additional UI updates to navigation and textinput for messenger.

--- src/Components/TextInput/Messenger.js
+++ src/Components/TextInput/Messenger.js
@@ -9,7 +9,9 @@ import {
   Dimensions,
   Keyboard,
   Animated,
-  TouchableWithoutFeedback, PlatformColor, Platform
+  TouchableWithoutFeedback,
+  PlatformColor,
+  Platform,
 } from 'react-native'
 import { Icon, Avatar } from 'react-native-elements'
 import Icons from '@/Theme/Icons'
@@ -40,27 +42,48 @@ const MessengerTextInput = ({ messageInput, setMessageInput, onSend }) => {
           }
         >
           {additionalOptionsVisible ? (
-            <Animated.View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-evenly' }}>
+            <Animated.View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'space-evenly',
+              }}
+            >
               <TouchableOpacity>
-                <Avatar source={icons.messenger_dot_icon} size={18} avatarStyle={{ tintColor: '#0099FF' }} />
+                {Platform.OS === 'android' ? (
+                  <Avatar
+                    source={icons.messenger_dot_icon}
+                    size={18}
+                    avatarStyle={{ tintColor: '#09F' }}
+                  />
+                ) : (
+                  <Svg viewBox="0 0 36 36" height={33} width={33}>
+                    <Path
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M18 29c6.075 0 11-4.925 11-11S24.075 7 18 7 7 11.925 7 18s4.925 11 11 11zm-1-16a1 1 0 112 0v3.75c0 .138.112.25.25.25H23a1 1 0 110 2h-3.75a.25.25 0 00-.25.25V23a1 1 0 11-2 0v-3.75a.25.25 0 00-.25-.25H13a1 1 0 110-2h3.75a.25.25 0 00.25-.25V13z"
+                      fill="#09F"
+                    />
+                  </Svg>
+                )}
               </TouchableOpacity>
               <TouchableOpacity>
                 <Icon
                   name={'camera'}
                   type={'ionicon'}
-                  size={25}
-                  color={'#0099FF'}
+                  size={27}
+                  color={'#09F'}
                 />
               </TouchableOpacity>
               <TouchableOpacity>
-                {/*<Icon name={'photo'} style={{ borderRadius: 10 }} type={'foundation'} size={30} color={'#0099FF'} />*/}
-                <Svg viewBox="0 0 36 36" height={33} width={33}>
-                  <Path d="M13.5 16.5a2 2 0 100-4 2 2 0 000 4z" fill="#0099FF" />
+                {/*<Icon name={'photo'} style={{ borderRadius: 10 }} type={'foundation'} size={30} color={'#09F'} />*/}
+                <Svg viewBox="0 0 36 36" height={35} width={35}>
+                  <Path d="M13.5 16.5a2 2 0 100-4 2 2 0 000 4z" fill="#09F" />
                   <Path
                     fillRule="evenodd"
                     clipRule="evenodd"
                     d="M7 12v12a4 4 0 004 4h14a4 4 0 004-4V12a4 4 0 00-4-4H11a4 4 0 00-4 4zm18-1.5H11A1.5 1.5 0 009.5 12v9.546a.25.25 0 00.375.217L15 18.803a6 6 0 016 0l5.125 2.96a.25.25 0 00.375-.217V12a1.5 1.5 0 00-1.5-1.5z"
-                    fill="#0099FF"
+                    fill="#09F"
                   />
                 </Svg>
               </TouchableOpacity>
@@ -68,12 +91,14 @@ const MessengerTextInput = ({ messageInput, setMessageInput, onSend }) => {
                 <Avatar
                   source={icons.facebook_microphone}
                   size={23}
-                  avatarStyle={{ tintColor: '#0099FF' }}
+                  avatarStyle={{ tintColor: '#09F' }}
                 />
               </TouchableOpacity>
             </Animated.View>
           ) : (
-            <Animated.View style={{ flexDirection: 'row', alignItems: 'center' }}>
+            <Animated.View
+              style={{ flexDirection: 'row', alignItems: 'center' }}
+            >
               <TouchableOpacity
                 style={styles.btnNavigation}
                 onPress={() => setAdditionalOptionsVisible(true)}
@@ -82,7 +107,7 @@ const MessengerTextInput = ({ messageInput, setMessageInput, onSend }) => {
                   name={'chevron-right'}
                   type={'feather'}
                   size={30}
-                  color={'#0099FF'}
+                  color={'#09F'}
                 />
               </TouchableOpacity>
             </Animated.View>
@@ -101,9 +126,11 @@ const MessengerTextInput = ({ messageInput, setMessageInput, onSend }) => {
               style={{
                 ...styles.msgInput,
               }}
-              selectionColor={'#0099FF'}
+              selectionColor={'#09F'}
               placeholderTextColor={'#65676b'}
-              placeholder={additionalOptionsVisible ? 'Aa' : 'Type a message...'}
+              placeholder={
+                additionalOptionsVisible ? 'Aa' : 'Type a message...'
+              }
             />
             <View style={styles.msgRightOptions}>
               <TouchableOpacity style={styles.btnNavigation}>
@@ -112,22 +139,24 @@ const MessengerTextInput = ({ messageInput, setMessageInput, onSend }) => {
                     fill-rule="evenodd"
                     clip-rule="evenodd"
                     d="M18 29c6.075 0 11-4.925 11-11S24.075 7 18 7 7 11.925 7 18s4.925 11 11 11zm-5.25-13c0-1.25.563-2 1.5-2 .938 0 1.5.75 1.5 2s-.563 2-1.5 2c-.938 0-1.5-.75-1.5-2zm7.5 0c0-1.25.563-2 1.5-2 .938 0 1.5.75 1.5 2s-.563 2-1.5 2c-.938 0-1.5-.75-1.5-2zm-7.52 5.464a1 1 0 011.41-.12 5.963 5.963 0 003.856 1.406c1.47 0 2.813-.528 3.856-1.406a1 1 0 111.288 1.53 7.962 7.962 0 01-5.144 1.876 7.962 7.962 0 01-5.144-1.877 1 1 0 01-.121-1.409z"
-                    fill="#0099FF"
+                    fill="#09F"
                   />
                 </Svg>
               </TouchableOpacity>
             </View>
           </View>
         </Col>
-        <Col style={{ width: 50 }}>
-          <TouchableOpacity style={{ alignSelf: 'center', justifyContent: 'center', flex: 1 }}>
+        <Col style={{ width: 55 }}>
+          <TouchableOpacity
+            style={{ alignSelf: 'center', justifyContent: 'center', flex: 1 }}
+          >
             {messageInput.length > 0 ? (
               <TouchableWithoutFeedback onPress={onSend}>
                 <View>
                   <Svg width="20px" height="20px" viewBox="0 0 24 24">
                     <Path
                       d="M16.6915026,12.4744748 L3.50612381,13.2599618 C3.19218622,13.2599618 3.03521743,13.4170592 3.03521743,13.5741566 L1.15159189,20.0151496 C0.8376543,20.8006365 0.99,21.89 1.77946707,22.52 C2.41,22.99 3.50612381,23.1 4.13399899,22.8429026 L21.714504,14.0454487 C22.6563168,13.5741566 23.1272231,12.6315722 22.9702544,11.6889879 C22.8132856,11.0605983 22.3423792,10.4322088 21.714504,10.118014 L4.13399899,1.16346272 C3.34915502,0.9 2.40734225,1.00636533 1.77946707,1.4776575 C0.994623095,2.10604706 0.8376543,3.0486314 1.15159189,3.99121575 L3.03521743,10.4322088 C3.03521743,10.5893061 3.34915502,10.7464035 3.50612381,10.7464035 L16.6915026,11.5318905 C16.6915026,11.5318905 17.1624089,11.5318905 17.1624089,12.0031827 C17.1624089,12.4744748 16.6915026,12.4744748 16.6915026,12.4744748 Z"
-                      fill="#0099FF"
+                      fill="#09F"
                       fill-rule="evenodd"
                       stroke="none"
                     />
@@ -138,7 +167,7 @@ const MessengerTextInput = ({ messageInput, setMessageInput, onSend }) => {
               <Svg viewBox="0 0 22 23" height="20" width="20">
                 <Path
                   d="M10.987 0c1.104 0 3.67.726 3.67 5.149 0 1.232-.123 2.001-.209 2.534a16.11 16.11 0 00-.048.314l-.001.005a.36.36 0 00.362.406c4.399 0 6.748 1.164 6.748 2.353 0 .533-.2 1.02-.527 1.395a.11.11 0 00.023.163 2.13 2.13 0 01.992 1.79c0 .86-.477 1.598-1.215 1.943a.11.11 0 00-.046.157c.207.328.329.713.329 1.128 0 .946-.547 1.741-1.406 2.029a.109.109 0 00-.068.137c.061.184.098.38.098.584 0 1.056-1.776 1.913-5.95 1.913-3.05 0-5.154-.545-5.963-.936-.595-.288-1.276-.81-1.276-2.34v-6.086c0-1.72.958-2.87 1.911-4.014C9.357 7.49 10.3 6.36 10.3 4.681c0-1.34-.091-2.19-.159-2.817-.039-.368-.07-.66-.07-.928 0-.527.385-.934.917-.936zM3.5 11h-2C.5 11 0 13.686 0 17s.5 6 1.5 6h2a1 1 0 001-1V12a1 1 0 00-1-1z"
-                  fill="#0099FF"
+                  fill="#09F"
                 />
               </Svg>
             )}
